6667ab5dded7d903750ca46f68e749a0af786e66
KuntaiAhn <103969679+KuntaiAhn@users.noreply.github.com>
2024-05-01 13:11:23 +0800

    Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
+    <main className="bg-gray-300 h-screen flex flex-col items-center justify-center p-5 gap-3">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-5">
         {['Kuntai', 'Melissa', 'Eunchan', 'Eunbi', 'yourself'].map(
           (person, index) => (
